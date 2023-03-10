a48a95c1093f785bace3f7265e1892c0732e51bc
kashishsinghyadav <117498422+kashishsinghyadav@users.noreply.github.com>
2023-03-10 16:46:45 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+
+        }else{
+            $('.navbar').removeClass("sticky");
+
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    //owl
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplaytimeout:true,
+        responsive: {
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            },
+
+        }
+
+    });
+    
+});
